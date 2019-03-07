f47bfba447bd8d4fe22cfff196e7407dd294e468
hadrien.george <Hadrien.GEORGE@akka.eu>
2019-03-07 10:55:36 +0100

    US#6: Change page favicon dinamycally with app theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { rootReducer } from './app.reducer';
 import { MatMenuModule, MatButtonModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { BROWSER_FAVICONS_CONFIG } from './core/services/color-picker.service';
+import { BrowserFavicons } from './core/services/color-picker.service';
+import { Favicons } from './core/services/color-picker.service';
+
 import { TodoService } from './core/services/todo.service';
 import { ColorPickerService } from './core/services/color-picker.service';
 import { ColorPickerComponent } from './pages/color-picker/color-picker.component';
@@ -40,7 +44,76 @@ import { SortTodosEffects } from './model/effects/sort-by-id.effects';
   ],
   providers: [
     TodoService,
-    ColorPickerService
+    ColorPickerService,
+    {
+      provide: Favicons,
+      useClass: BrowserFavicons
+    },
+    {
+      provide: BROWSER_FAVICONS_CONFIG,
+      useValue: {
+          icons: {
+              greenlight: {
+                  type: 'image/x-icon',
+                  href: './assets/greenlight.ico',
+                  isDefault: true
+              },
+              redlight: {
+                  type: 'image/x-icon',
+                  href: './assets/redlight.ico'
+              },
+              reddark: {
+                type: 'image/x-icon',
+                href: './assets/reddark.ico'
+              },
+              yellow: {
+                type: 'image/x-icon',
+                href: './assets/yellow.ico'
+              },
+              orange: {
+                type: 'image/x-icon',
+                href: './assets/orange.ico'
+              },
+              greendark: {
+                type: 'image/x-icon',
+                href: './assets/greendark.ico'
+              },
+              cyanlight: {
+                type: 'image/x-icon',
+                href: './assets/cyanlight.ico'
+              },
+              cyandark: {
+                type: 'image/x-icon',
+                href: './assets/cyandark.ico'
+              },
+              bluelight: {
+                type: 'image/x-icon',
+                href: './assets/bluelight.ico'
+              },
+              bluedark: {
+                type: 'image/x-icon',
+                href: './assets/bluedark.ico'
+              },
+              purplelight: {
+                type: 'image/x-icon',
+                href: './assets/purplelight.ico'
+              },
+              purpledark: {
+                type: 'image/x-icon',
+                href: './assets/purpledark.ico'
+              },
+              grey: {
+                type: 'image/x-icon',
+                href: './assets/grey.ico'
+              },
+              black: {
+                type: 'image/x-icon',
+                href: './assets/black.ico'
+              },
+          },
+          cacheBusting: true
+      }
+    }
   ],
   bootstrap: [AppComponent]
 })
